11d125156267191d44bc1a739fe2ddcb29b0bd20
Anas Hany <anashany2193@gmail.com>
2025-01-28 01:52:06 +0200

    Implement Order Page Components

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 
+import Header from "./../components/Header";
+import { contextMenuItems, ordersData, ordersGrid } from "./../data/dummy";
+
+import {
+  GridComponent,
+  ColumnDirective,
+  ColumnsDirective,
+  Resize,
+  Sort,
+  Filter,
+  ContextMenu,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
+
 const Orders = () => {
-  return <div>Orders</div>;
+  return (
+    <div className="p-2 m-2 bg-white md:m-9 md:p-9 rounded-3xl">
+      <Header title="Orders" category="Page" />
+      <GridComponent allowPaging allowSorting dataSource={ordersData}>
+        <ColumnsDirective>
+          {ordersGrid.map((order, i) => (
+            <ColumnDirective key={i} {...order}></ColumnDirective>
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            Page,
+            ContextMenu,
+            Filter,
+            ExcelExport,
+            Edit,
+            PdfExport,
+          ]}
+        />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Orders;
