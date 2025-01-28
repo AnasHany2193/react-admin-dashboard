1ee7d1e341346b5cae2a311e740ec7dc9358c4d1
Anas Hany <anashany2193@gmail.com>
2025-01-28 14:34:42 +0200

    Implement Editor Page

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Header from "../components/Header";
+
+import { EditorData } from "../data/dummy";
+
+import {
+  HtmlEditor,
+  Image,
+  Inject,
+  Link,
+  QuickToolbar,
+  RichTextEditorComponent,
+  Toolbar,
+} from "@syncfusion/ej2-react-richtexteditor";
 
 const Editor = () => {
-  return <div>Editor</div>;
+  return (
+    <div className="p-2 m-2 bg-white md:m-9 md:p-9 rounded-3xl">
+      <Header title="Editor" category="App" />
+      <RichTextEditorComponent>
+        <EditorData />
+        <Inject services={[Toolbar, Image, Link, HtmlEditor, QuickToolbar]} />
+      </RichTextEditorComponent>
+    </div>
+  );
 };
 
 export default Editor;
